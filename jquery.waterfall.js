11d862012854a3f59070fc51921980d28620d8b2
Dmitry <df.creative@gmail.com>
2013-02-01 23:40:00 +0400

    Initial commit

---
+++ jquery.waterfall.js
@@ -0,0 +1,135 @@
+/*
+Simple min-height-masonry layout plugin.
+Like masonry column shift, but works.
+*/
+(function ($){
+	var Waterfall = function (el, opts){
+		this.element = $(el);
+		this._create(opts)
+	}
+
+	$.extend(Waterfall.prototype, {
+		options: {
+			itemSelector: null,
+			colMinWidth: 200,
+			defaultContainerWidth: $(window).width(),
+			colClass: null
+		},
+
+		_create: function (opts) {
+			var self = this,
+			o = self.options = $.extend({}, self.options, opts);
+
+			self.container = self.element;
+			self.items = o.itemSelector ? $(o.itemSelector, self.container) : self.container.children();
+			self.items.detach();
+
+			//save min width
+			o.colMinWidth = parseInt(self.items.css("min-width")) || o.colMinWidth;
+
+			self.reflow();
+		},
+
+		//==========================API
+		//Ensures column number correct, reallocates items
+		reflow: function () {
+			var self = this, o = self.options,
+				neededCols = self._countNeededColumns();
+
+			if (neededCols == self.container.children().length) return;
+			self.items.detach();
+			self._ensureColumns(neededCols)._refill();
+
+			return self;
+		},
+
+		//Inserts new item(s)
+		add: function (itemSet) {
+			var self = this, o = self.options, cols = self.container.children();
+
+			itemSet.each(function (i, el) {
+				var $item = $(el);
+				self._getMinCol(cols).append($item);
+			})
+
+			self.items = self.items.add(itemSet);
+			return self;
+		},
+
+
+		//========================= Techs
+
+		//calc needed number of columns
+		_countNeededColumns: function () {
+			var self = this, o = self.options;
+			return ~~((self.container.width() || o.defaultContainerWidth) / o.colMinWidth) || 1;
+		},
+
+		//ensures only number of columns exist
+		_ensureColumns: function (num) {
+			var self = this, o = self.options
+				num = num || 1,
+				columns = self.container.children();
+
+			if (columns.length < num) {
+				for (var i = 0; i < num - columns.length; i++ ){
+					self.container.append(self._columnTpl());
+				}
+			} else if ( columns.length > num) {
+				columns.slice(- columns.length + num).remove();
+			}
+
+			columns = self.container.children();
+			columns.css({
+				"width": 100 / columns.length +"%",
+				"display": "inline-block",
+				"vertical-align": "top"
+			})
+
+			return self;
+		},
+
+		_columnTpl: function () {
+			return '<div class="wf-column ' + (this.options.colClass || '')  + '"></div>';
+		},
+
+		//Redistributes items by columns
+		_refill: function () {
+			var self = this, o = self.options;
+
+			//for each item place it correctly
+			self.items.each(function (i, el) {
+				self._getMinCol(self.container.children()).append($(el));
+			})
+
+			return self;
+		},
+
+		//returns column with minimal height
+		_getMinCol: function (cols) {
+			var minH = Infinity, minCol = cols.first(), minColNum = 0;
+
+			//fill min heights
+			cols.each(function (colNum, col){
+				var $col = $(col);
+
+				var h = $col.height();
+				if (h < minH) {
+					minH = h;
+					minColNum = colNum;
+				}
+			});
+
+			return cols.eq(minColNum);
+		},
+
+		
+	})
+
+
+	$.fn.waterfall = function (opts) {
+		return $(this).each(function (i, el) {
+			if (!$(el).data("waterfall")) $(el).data("waterfall", new Waterfall(el, opts));
+		})
+	}
+})(jQuery)
---
+++ readme.md
@@ -0,0 +1,56 @@
+# jQuery.waterfall 
+
+jQuery.waterfall is straightforward <a href="http://pinterest.com">pinterest</a>-like layout with fluid width of columns. The primary goal was to create fast, lightweight, bugproof and free alternative for <a href="http://isotope.metafizzy.co/custom-layout-modes/masonry-column-shift.html">isotope masonry column shift</a> layout.
+
+### Use
+
+```javascript
+<script src="js/jquery.js"></script>
+<script src="js/jquery.masonry.js"></script>
+
+<script>
+//To make 
+$(function () {
+	$('.items-container').waterfall({
+		itemSelector: '.item', //by default – children of container
+		colMinWidth: 200, //px, by default – 200
+		defaultContainerWidth: //Container may be hidden, so it’s necessary to set default width. By default – $(window).width(),
+		colClass: null //Class to append to columns
+	})
+});
+</script>
+
+<div class="items-container">
+	<div class="item">Item 1</div>
+	<div class="item">Item 2</div>
+	<div class="item">Item 3</div>
+</div>
+```
+
+### Methods
+
+To get waterfall instance:
+```javascript
+var waterfall = $('.items-container').data('waterfall');
+```
+
+`waterfall.reflow()` recounts needed number of columns, redistributes items. Speed is optimized, so it takes minimal possible calcs.
+There’s a sense to use this if resize happened.
+
+
+`waterfall.add(item)` appends new item or items to layout, use this instead of `container.append(item)`.
+Waterfall will take care of optimal placing and appending
+
+```javascript
+waterfall.add($('<div class="item">Some item.</div>'));
+waterfall.add($('<div class="item">Item 1.</div><div class="item">Item 2.</div>'));
+```
+
+### Principle
+
+Plugin creates fluid columns according to `colMinWidth` param and carefully fills them with items.
+
+
+### TODO
+* demo
+
